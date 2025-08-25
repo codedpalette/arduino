7fbba528f40dc2605de47dee41c360e450d25e88
daniel <daniel.blindr@gmail.com>
2025-08-25 21:10:53 +0300

    Color selection and clearing

--- src/screen.cpp
+++ src/screen.cpp
@@ -8,32 +8,70 @@ Waveshare_ILI9486 Waveshield;
 #define WIDTH 480
 #define HEIGHT 320
 
-#define BLACK 0x0000
-#define BLUE 0x001F
-#define RED 0xF800
-#define GREEN 0x07E0
-#define CYAN 0x07FF
-#define MAGENTA 0xF81F
-#define YELLOW 0xFFE0
-#define WHITE 0xFFFF
+#define BUTTON_PIN 2
+#define PIN_RED A3
+#define PIN_GREEN A4
+#define PIN_BLUE A5
+
+#define MAX_POTENTIOMETER_VALUE 906
 
 void setup() {
+  Serial.begin(9600);
   SPI.begin();
+
   Waveshield.begin();
+  Waveshield.setRotation(1);
+
+  pinMode(BUTTON_PIN, INPUT);
 }
 
+uint16_t getColor(uint8_t red, uint8_t green, uint8_t blue);
+void debugColor(uint8_t red, uint8_t green, uint8_t blue);
+void debugTsConfigData(TSConfigData data);
+
 void loop() {
-  Waveshield.setRotation(1);
-  // Waveshield.fillScreen(RED);
-  // Waveshield.fillCircle(WIDTH / 2, HEIGHT / 2, HEIGHT / 2, BLUE);
-  // delay(1000);
+  uint8_t red =
+      uint8_t(map(analogRead(PIN_RED), 0, MAX_POTENTIOMETER_VALUE, 0, 255));
+  uint8_t green =
+      uint8_t(map(analogRead(PIN_GREEN), 0, MAX_POTENTIOMETER_VALUE, 0, 255));
+  uint8_t blue =
+      uint8_t(map(analogRead(PIN_BLUE), 0, MAX_POTENTIOMETER_VALUE, 0, 255));
+  uint16_t color = getColor(red, green, blue);
+  // debugColor(red, green, blue);
 
-  // Get raw touchscreen values.
-  TSPoint p = Waveshield.getPoint();
+  TSConfigData data = Waveshield.getTsConfigData();
+  debugTsConfigData(data);
 
-  // Remaps raw touchscreen values to screen co-ordinates.  Automatically
-  // handles rotation!
-  Waveshield.normalizeTsPoint(p);
+  int buttonState = digitalRead(BUTTON_PIN);
+  if (buttonState == HIGH) {
+    Waveshield.fillScreen(0x0000);
+  } else {
+    TSPoint p = Waveshield.getPoint();
+    Waveshield.normalizeTsPoint(p);
+    Waveshield.fillCircle(p.x, p.y, 3, color);
+  }
+}
 
-  Waveshield.fillCircle(p.x, p.y, 3, BLUE);
+void debugTsConfigData(TSConfigData data) {
+  Serial.print("Xmin: ");
+  Serial.print(data.xMin);
+  Serial.print(", Xmax: ");
+  Serial.print(data.xMax);
+  Serial.print(", Ymin: ");
+  Serial.print(data.yMin);
+  Serial.print(", Ymax: ");
+  Serial.println(data.yMax);
+}
+
+void debugColor(uint8_t red, uint8_t green, uint8_t blue) {
+  Serial.print("Red: ");
+  Serial.print(red);
+  Serial.print(", Green: ");
+  Serial.print(green);
+  Serial.print(", Blue: ");
+  Serial.println(blue);
+}
+
+uint16_t getColor(uint8_t red, uint8_t green, uint8_t blue) {
+  return (red << 11) | (green << 5) | blue;
 }
